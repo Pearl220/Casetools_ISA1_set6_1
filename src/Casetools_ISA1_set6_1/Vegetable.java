26a0b8188c01c040fa5c2001548835b60aaf487f
pearl <pearlfeanands0631@gmail.com>
2024-02-10 10:51:07 +0530

    commit

---
+++ src/Casetools_ISA1_set6_1/Vegetable.java
@@ -0,0 +1,20 @@
+package Casetools_ISA1_set6_1;
+
+public class Vegetable {
+
+		String name;
+		Vegetable(String name)
+		{
+			this.name=name;
+		}
+		public static void main(String[]args) {
+			//TODO Auto-generated method stub
+			Vegetable spinach=new Vegetable ("spinach");
+			System.out.println("Vegetable is"+spinach .name);
+			
+
+
+		}
+
+
+}
